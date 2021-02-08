64fb11ca7d38749c7972c260712e8471271843f7
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-02-08 15:34:08 -0300

    deploy front

---
+++ precache-manifest.5eeb8d5612048ac5aeacbe78ed6f7a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278fa50aa52b316d8607",
+    "url": "css/app.ca176a87.css"
+  },
+  {
+    "revision": "0da110e97eceeedfa3dde7fe1a942c71",
+    "url": "index.html"
+  },
+  {
+    "revision": "278fa50aa52b316d8607",
+    "url": "js/app.b6f8b30d.js"
+  },
+  {
+    "revision": "0dc82092b362b242d205",
+    "url": "js/chunk-vendors.d7468477.js"
+  },
+  {
+    "revision": "501a854472195e46c1b5a2b4dfba5c59",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
